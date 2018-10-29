89028c252afe8ca4ac4192823be25832437f77ce
ilpise <pisewip@yahoo.it>
2018-10-29 08:20:02 +0100

    Added modules and themes

--- sites/all/modules/multiple_email/multiple_email.api.php
+++ sites/all/modules/multiple_email/multiple_email.api.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- *  API functions for Multiple Email module.
+ * API functions for Multiple Email module.
  */
 
 /**
@@ -14,17 +14,19 @@
  * Signal that an email address has been registered by Multiple Email.
  *
  * @param $email
- *  The fully-loaded email object created by Multiple Email.
+ *   The fully-loaded email object created by Multiple Email.
  */
 function hook_multiple_email_register($email) {
   drupal_set_message(t('Do not forget to confirm %email!', array('%email' => $email->mail)));
 }
 
 /**
- * Signal that an email address has been confirmed by Multiple Email.
+ * Signal that an email address has been confirmed, or un-confirmed.
+ *
+ * Check $email->confirmed to determine if the email was confirmed or not.
  *
  * @param $email
- *  The fully-loaded email object created by Multiple Email.
+ *   The fully-loaded email object created by Multiple Email.
  */
 function hook_multiple_email_confirm($email) {
   drupal_set_message(t('Now that %email has been confirmed, you may make it your primary address.', array('%email' => $email->mail)));
@@ -34,11 +36,12 @@ function hook_multiple_email_confirm($email) {
  * Signal that an email address has been deleted by Multiple Email.
  *
  * When an email address is deleted by multiple email, this hook is invoked to
- * notify other modules this has happened. Addresses might also be deleted
- * in multiple_email's implementation of hook_user(), without triggering this hook.
+ * notify other modules this has happened. Addresses might also be deleted in
+ * multiple_email's implementation of hook_user(), without triggering this
+ * hook.
  *
  * @param $eid
- *  Email Address ID.
+ *   Email Address ID.
  */
 function hook_multiple_email_delete($eid) {
   db_query("DELETE FROM {mytable} WHERE eid = %d", $eid);
